9cf140750f1509074854b6173daea1322a5f465b
Teagan Smith <teagan.ms.ut@gmail.com>
2025-11-30 12:29:14 -0700

    Updated format of Discord mesage

--- discord_channel.py
+++ discord_channel.py
@@ -1,8 +1,15 @@
 from discord_webhook import DiscordWebhook
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def send_discord_message(webhook_url, embed_data):
     
-    webhook = DiscordWebhook(url=webhook_url, content="Daily Flight Tracker")
+    tz = ZoneInfo("America/Denver")
+    now = datetime.now(tz)
+
+    message = now.strftime("Lowest Flights as of %b %-d, %-I %p")
+    
+    webhook = DiscordWebhook(url=webhook_url, content=message)
 
     webhook.add_embed(embed_data)
 
